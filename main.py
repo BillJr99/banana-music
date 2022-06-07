a47b10dfe6e54b10eacd4879b17cdea9ef224fe4
BillJr99 <BillJr99@users.noreply.github.com>
2022-06-07 10:25:37 -0400

    Math

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ prior = 0
 def on_every_interval():
     global current, prior
     current = pins.analog_read_pin(AnalogPin.P1)
-    current = pins.map(current, 128, 896, 0, 36) * 23.6389 + 137
-    if abs(current - prior) > 0:
-        music.play_tone(current, music.beat(BeatFraction.WHOLE))
+    current = pins.map(current, 0, 1023, 0, 3)
+    if abs(current - prior) > 0.01:
+        music.play_tone(min(max(pins.map(current, 0.1, 0.25, 0, 36), 0), 36) * 23.6389 + 137,
+            music.beat(BeatFraction.WHOLE))
     prior = current
-    basic.show_number(pins.map(current, 128, 896, 0, 3))
+    basic.show_number(current)
 loops.every_interval(1000, on_every_interval)
